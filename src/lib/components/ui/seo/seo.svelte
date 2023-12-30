d2e73bed39545d368612f2a40425237632a46fc8
gernii <anhducngole2001@gmail.com>
2023-12-30 21:24:46 +0700

    fix: :bug: Update SEO and language routing
    This commit updates the SEO component in the `seo.svelte` file to use the `PUBLIC_HOSTNAME` environment variable for the canonical URL and image URL.
    It also adds a new `urlPathName` variable to handle the case when the URL pathname is `/`.
    Additionally, it updates the `link` and `openGraph` tags in the `<svelte:head>` section to use the `hostname` and `urlPathName` variables.
    In the `+layout.svelte` file, it adds a new `<link>` tag for the Vietnamese language version using the `hostname` and `route` functions. The `manifest.json` file is updated to change the app name to "CoTAI Education".
    The `svelte.config.js` and `vite.config.ts` files are modified to remove the unused `loadEnv` function and update the `routes` configuration in the Vite adapter.

--- src/lib/components/ui/seo/seo.svelte
+++ src/lib/components/ui/seo/seo.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
+	import { PUBLIC_HOSTNAME } from '$env/static/public';
+
 	import SvelteSeo from 'svelte-seo';
 
 	import * as m from '$i18n/messages';
@@ -11,16 +13,22 @@
 		SEOImage?: string;
 	}
 
+	$: urlPathName = $page.url.pathname !== '/' ? $page.url.pathname : '';
+
 	export let title: $$Props['title'] = undefined;
 	export let description: $$Props['description'] = undefined;
-	export let SEOImage: $$Props['SEOImage'] = `${$page.url.origin}/images/logo/CoTAI-Ver0-320.png`;
+	export let SEOImage: $$Props['SEOImage'] = undefined;
+
+	let hostname = PUBLIC_HOSTNAME;
+
+	$: imageUrl = SEOImage ?? `${hostname}/images/logo/CoTAI-Ver0-320.png`;
 
 	$: contentTitle = title ? m.siteNameWithTitle({ title }) : m.siteName();
 </script>
 
 <svelte:head>
-	<link rel="canonical" href={$page.url.href} />
-	<link rel="manifest" href={`${$page.url.origin}/manifest.json`} />
+	<link rel="canonical" href={`${hostname}${urlPathName}`} />
+	<link rel="manifest" href="/manifest.json" />
 </svelte:head>
 <SvelteSeo
 	title={contentTitle}
@@ -28,12 +36,12 @@
 	openGraph={{
 		title: contentTitle,
 		type: 'website',
-		url: $page.url.href,
+		url: `${hostname}${urlPathName}`,
 		site_name: m.siteName(),
 		description,
 		images: [
 			{
-				url: SEOImage,
+				url: imageUrl,
 				alt: m.siteName()
 			}
 		]
@@ -42,32 +50,13 @@
 		card: 'summary_large_image',
 		title: contentTitle,
 		description,
-		image: SEOImage
+		image: imageUrl
 	}}
 	jsonLd={{
 		'@context': 'https://schema.org',
 		'@type': 'WebSite',
-		url: $page.url.origin,
+		url: hostname,
 		description: m.homePage_head_description(),
 		name: m.siteName()
 	}}
 />
-<!-- extend={{
-    // extending the default link tags
-    link: [
-        { rel: 'icon', href: '/favicon.ico', type: 'image/png' },
-        { rel: 'apple-touch-icon', href: '/favicon.ico', type: 'image/png' },
-        { rel: 'apple-touch-startup-image', href: '/favicon.ico', type: 'image/png' }
-    ],
-    meta: [
-        {
-            name: 'apple-mobile-web-app-capable',
-            content: 'yes'
-        },
-        {
-            name: 'apple-mobile-web-app-status-bar-style',
-            content: 'black'
-        }
-    ]
-    // extending the default meta tags
-}} -->
--- src/routes/(landing-page)/[[lang=lang]]/+layout.svelte
+++ src/routes/(landing-page)/[[lang=lang]]/+layout.svelte
@@ -1,35 +1,31 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { browser } from '$app/environment';
+
+	import { PUBLIC_HOSTNAME } from '$env/static/public';
 
 	import { LandingPageLayout } from '$lib/layouts/landing-page';
 
 	import { currentLang, route } from '$lib/lib/i18n/routing';
 
-	import {
-		availableLanguageTags,
-		setLanguageTag,
-		type AvailableLanguageTag,
-		sourceLanguageTag
-	} from '$i18n/runtime';
+	let hostname = PUBLIC_HOSTNAME;
 
-	//Use the default language if no language is given
-	$: lang = ($page.params.lang as AvailableLanguageTag) ?? sourceLanguageTag;
-	$: setLanguageTag(lang);
+	// TODO: Update after implement ENG
+	// Use the default language if no language is given
+	// $: lang = ($page.params.lang as AvailableLanguageTag) ?? sourceLanguageTag;
+	// $: setLanguageTag(lang);
 
-	//Set the lang attribute on the html tag
-	$: if (browser) document.documentElement.lang = lang;
+	// Set the lang attribute on the html tag
+	// $: if (browser) document.documentElement.lang = lang;
 </script>
 
-<svelte:head>
+<link rel="alternate" hreflang={'vi'} href={`${hostname}${route($page.url.pathname, 'vi')}`} />
+
+<!-- TODO: Update after implement ENG -->
+<!-- <svelte:head>
 	{#each availableLanguageTags as lang}
-		<link
-			rel="alternate"
-			hreflang={lang}
-			href={`${$page.url.origin}${route($page.url.pathname, lang)}`}
-		/>
+		<link rel="alternate" hreflang={lang} href={`${hostname}${route($page.url.pathname, lang)}`} />
 	{/each}
-</svelte:head>
+</svelte:head> -->
 
 {#key $currentLang}
 	<LandingPageLayout>
