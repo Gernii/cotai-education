7b351352f5fb70550ee7af998dbab340edd46583
gernii <anhducngole2001@gmail.com>
2023-12-30 13:12:39 +0700

    fix: :ambulance: fix sitemap and robots generator

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { paraglide } from '@inlang/paraglide-js-adapter-vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { loadEnv } from 'vite';
-import sitemap from 'vite-plugin-sitemap';
 // @ts-ignore
 export default ({ mode }) => {
 	process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
@@ -13,17 +12,6 @@ export default ({ mode }) => {
 			paraglide({
 				project: './project.inlang',
 				outdir: './src/lib/lib/i18n/messages'
-			}),
-			sitemap({
-				hostname: process.env.VITE_HOSTNAME,
-				outDir: '.svelte-kit/output/client',
-				robots: [
-					{
-						userAgent: '*',
-						allow: ['/'],
-						disallow: ['/admin', '/private', '/*.js$', '/*.json', '/en']
-					}
-				]
 			})
 		],
 		test: {
