41944e160bc11ba67319488225e5b0791ba86f97
gernii <anhducngole2001@gmail.com>
2024-06-20 16:21:38 +0700

    feat(web): :lipstick: add AOS to course details page

--- apps/web/src/lib/pages/course-details/curriculum/section.svelte
+++ apps/web/src/lib/pages/course-details/curriculum/section.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
+	import { writable } from 'svelte/store';
+	import { inview } from 'svelte-inview';
+
 	import { Container } from '$lib/components/ui/container';
 	import ContainerContent from '$lib/components/ui/container/container-content.svelte';
 	import SectionTitle from '$lib/components/ui/section-title/section-title.svelte';
+
+	import { inviewCommonOptions, onInViewEnter } from '$lib/libs/inview';
 
 	import * as m from '$i18n/messages';
 
@@ -11,21 +16,27 @@
 
 	import CurriculumCard from './curriculum-card.svelte';
 
+	let isInview = writable(false);
+
 	$: pd = $page.data as CourseDetailsPageDataProps;
 
 	$: curriculum = pd.curriculum;
 </script>
 
 {#if curriculum.length > 0}
-	<Container>
-		<ContainerContent>
-			<SectionTitle>{m.curriculum()}</SectionTitle>
+	<section use:inview={inviewCommonOptions} on:inview_enter={onInViewEnter(isInview)}>
+		<Container>
+			<ContainerContent>
+				<div class:opacity-0={!$isInview} class:animate-fade-left={$isInview}>
+					<SectionTitle>{m.curriculum()}</SectionTitle>
+				</div>
 
-			<ul class="space-y-4">
-				{#each curriculum as section}
-					<CurriculumCard {...section} />
-				{/each}
-			</ul>
-		</ContainerContent>
-	</Container>
+				<ul class="space-y-4" class:opacity-0={!$isInview} class:animate-fade-up={$isInview}>
+					{#each curriculum as section}
+						<CurriculumCard {...section} />
+					{/each}
+				</ul>
+			</ContainerContent>
+		</Container>
+	</section>
 {/if}
--- apps/web/src/lib/pages/course-details/learning-outcomes/section.svelte
+++ apps/web/src/lib/pages/course-details/learning-outcomes/section.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
+	import { writable } from 'svelte/store';
+	import { inview } from 'svelte-inview';
+
 	import { Container, ContainerContent } from '$lib/components/ui/container';
 	import { SectionTitle } from '$lib/components/ui/section-title';
+
+	import { inviewCommonOptions, onInViewEnter } from '$lib/libs/inview';
 
 	import * as m from '$i18n/messages';
 
@@ -10,20 +15,26 @@
 
 	import Archives from './archives.svelte';
 
+	let isInview = writable(false);
+
 	$: pd = $page.data as CourseDetailsPageDataProps;
 
 	$: archives = pd.archives;
 </script>
 
 {#if archives && archives.length > 0}
-	<Container>
-		<ContainerContent>
-			<SectionTitle class="text-center">{m.learningoutcomes()}</SectionTitle>
-			<div class="space-y-8">
-				{#each archives as archiveGroup}
-					<Archives archives={archiveGroup} />
-				{/each}
-			</div>
-		</ContainerContent>
-	</Container>
+	<section use:inview={inviewCommonOptions} on:inview_enter={onInViewEnter(isInview)}>
+		<Container>
+			<ContainerContent>
+				<div class:opacity-0={!$isInview} class:animate-fade-down={$isInview}>
+					<SectionTitle class="text-center">{m.learningoutcomes()}</SectionTitle>
+				</div>
+				<div class="space-y-8" class:opacity-0={!$isInview} class:animate-fade-up={$isInview}>
+					{#each archives as archiveGroup}
+						<Archives archives={archiveGroup} />
+					{/each}
+				</div>
+			</ContainerContent>
+		</Container>
+	</section>
 {/if}
