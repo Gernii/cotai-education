711345598cfd28a855e384199cf6ba5218e0896b
Le Ngo Duc Anh <anhducngole2001@gmail.com>
2024-07-10 17:31:48 +0700

    perf(web): remove deprecated components (#41)

--- apps/web/src/lib/pages/course-details/page.svelte
+++ apps/web/src/lib/pages/course-details/page.svelte
@@ -3,10 +3,8 @@
 
     import Components from "./components.svelte";
     import { Curriculum } from "./curriculum";
-    import { ExperienceRequirement } from "./experience-requirement";
     import { WhoShouldJoin } from "./who-should-join";
     import { Hero } from "./hero";
-    import { LearningOutcomes } from "./learning-outcomes";
     import { OtherCourses } from "./other-courses";
     import { FAQ } from "./fag";
     import { Certs } from "./certs";
@@ -24,8 +22,8 @@
 
 <Hero />
 <WhoShouldJoin />
-<ExperienceRequirement />
-<LearningOutcomes />
+<!-- <ExperienceRequirement /> -->
+<!-- <LearningOutcomes /> -->
 <Components />
 <Curriculum />
 <Certs />
