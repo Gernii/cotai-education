cb1802b3c3d81ec5d39b593048bb2d007dc8fdb6
Le Ngo Duc Anh <anhducngole2001@gmail.com>
2024-07-10 23:56:21 +0700

    perf(web): improve curriculum collapsible (#42)

--- apps/web/src/lib/pages/course-details/curriculum/curriculum-card.svelte
+++ apps/web/src/lib/pages/course-details/curriculum/curriculum-card.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    import { createCollapsible, melt } from "@melt-ui/svelte";
     import { cx } from "cva";
+    import { slide } from "svelte/transition";
 
     import type { CurriculumProps } from "$lib/utils/types/data.deprecated";
 
@@ -8,43 +10,51 @@
 
     interface $$Props extends CurriculumProps {
         highlight?: boolean;
+        forceOpen?: boolean;
     }
 
     export let title: $$Props["title"] = undefined;
     export let details: $$Props["details"] = undefined;
     export let highlight: $$Props["highlight"] = undefined;
-
-    let isContentOpen = true;
-
-    const contentOpenToggle = () => {
-        isContentOpen = !isContentOpen;
-    };
+    export let forceOpen: $$Props["forceOpen"] = undefined;
 
     $: isContentEnabled = (details && !details.hidden) || $$slots.default;
+
+    const {
+        elements: { root, content, trigger },
+        states: { open },
+    } = createCollapsible({
+        forceVisible: true,
+        defaultOpen: forceOpen ?? false,
+    });
 </script>
 
 <li
-    class={cx("collapse border border-base-content/5", {
-        "collapse-open": isContentOpen,
-        "collapse-close": !isContentOpen,
-    })}
+    use:melt={$root}
+    class="overflow-hidden rounded-box border border-base-content/5"
 >
     <div
-        class="collapse-title flex items-center justify-between bg-base-100 pe-4 text-xl font-medium"
+        class="flex items-center justify-between bg-base-100 px-6 py-4 pe-4 text-xl font-medium"
     >
         <span class:text-primary={highlight}>{title}</span>
         {#if isContentEnabled}
             <button
                 class="btn btn-square btn-ghost btn-sm"
-                on:click={contentOpenToggle}
+                use:melt={$trigger}
+                aria-label="Xem nội dung"
             >
-                <HeroiconsChevronDown class="size-6 stroke-2" />
+                <HeroiconsChevronDown
+                    class={cx("size-6 transform", {
+                        "rotate-180": $open,
+                        "rotate-0": !$open,
+                    })}
+                />
             </button>
         {/if}
     </div>
-    {#if isContentEnabled}
-        <div class="collapse-content bg-base-200">
-            <div class="pt-4">
+    {#if isContentEnabled && $open}
+        <div class="bg-base-200" use:melt={$content} transition:slide>
+            <div class="py-4">
                 {#if $$slots.default}
                     <slot />
                 {:else if details}
