d797e67cc6eb6830f556106e7d975728694f8eca
Le Ngo Duc Anh <anhducngole2001@gmail.com>
2024-07-11 11:11:07 +0700

    feat(web): update new courses thumbnails (#55)
    ref #54

--- apps/web/src/lib/utils/courses-thumbnail.ts
+++ apps/web/src/lib/utils/courses-thumbnail.ts
@@ -1,18 +1,19 @@
-import CourseThumbnailMathCodeAIPicture from "$lib/assets/images/course-thumbnail-math-code-ai.jpg?imagetools";
-import CourseThumbnailPythonAIPicture from "$lib/assets/images/course-thumbnail-python-ai.jpg?imagetools";
+import CourseThumbnailMC4AIPicture from "$lib/assets/images/course-thumbnail/mc4ai.jpg?imagetools";
+import CourseThumbnailPythonAIPicture from "$lib/assets/images/course-thumbnail/pythonai.jpg?imagetools";
 import CourseThumbnailDefaultPicture from "$lib/assets/images/course-thumbnail-default.png?imagetools";
-import CourseThumbnailDeepLearingPicture from "$lib/assets/images/course-thumbnail-deep-learning.jpg?imagetools";
-import CourseThumbnailMachineLearingPicture from "$lib/assets/images/course-thumbnail-machine-learing.jpg?imagetools";
+import CourseThumbnailDL4CVPicture from "$lib/assets/images/course-thumbnail/dl4cv.jpg?imagetools";
+import CourseThumbnailDL4NLPPicture from "$lib/assets/images/course-thumbnail/dl4nlp.jpg?imagetools";
+import CourseThumbnailML4AIPicture from "$lib/assets/images/course-thumbnail/ml4ai.jpg?imagetools";
 
 import { coursesIdDeprecated, type CourseIdDeprecated } from "./constants";
 import { CourseIds } from "$lib/datas/courses/constants";
 
 export const coursesThumbnailDeprecated = (id: CourseIdDeprecated) => {
     const thumbnails = new Map<string, ImgMeta[] | ImgMeta>([
-        [coursesIdDeprecated[1], CourseThumbnailDeepLearingPicture],
+        [coursesIdDeprecated[1], CourseThumbnailDL4CVPicture],
         [coursesIdDeprecated[2], CourseThumbnailPythonAIPicture],
-        [coursesIdDeprecated[3], CourseThumbnailMathCodeAIPicture],
-        [coursesIdDeprecated[4], CourseThumbnailMachineLearingPicture],
+        [coursesIdDeprecated[3], CourseThumbnailMC4AIPicture],
+        [coursesIdDeprecated[4], CourseThumbnailML4AIPicture],
     ]);
 
     const img = thumbnails.get(id) ?? CourseThumbnailDefaultPicture;
@@ -23,10 +24,10 @@ export const coursesThumbnailDeprecated = (id: CourseIdDeprecated) => {
 export const coursesThumbnail = (id: CourseIds) => {
     const thumbnails = new Map<string, ImgMeta[] | ImgMeta>([
         [CourseIds.pythonai, CourseThumbnailPythonAIPicture],
-        [CourseIds.mc4ai, CourseThumbnailMathCodeAIPicture],
-        [CourseIds.ml4ai, CourseThumbnailMachineLearingPicture],
-        [CourseIds.dl4cv, CourseThumbnailDeepLearingPicture],
-        [CourseIds.dl4nlp, CourseThumbnailDeepLearingPicture],
+        [CourseIds.mc4ai, CourseThumbnailMC4AIPicture],
+        [CourseIds.ml4ai, CourseThumbnailML4AIPicture],
+        [CourseIds.dl4cv, CourseThumbnailDL4CVPicture],
+        [CourseIds.dl4nlp, CourseThumbnailDL4NLPPicture],
     ]);
 
     const img = thumbnails.get(id) ?? CourseThumbnailDefaultPicture;
