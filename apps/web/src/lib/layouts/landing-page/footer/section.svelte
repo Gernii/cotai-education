82c2e9b24df310dc86e1be56f7d4a6417b64c557
Le Ngo Duc Anh <anhducngole2001@gmail.com>
2024-07-16 14:37:01 +0700

    refactor(web): layout landing page footer (#78)

--- apps/web/src/lib/layouts/landing-page/footer/section.svelte
+++ apps/web/src/lib/layouts/landing-page/footer/section.svelte
@@ -15,7 +15,6 @@
 
     import LucideGem from "~icons/lucide/gem";
     import LucideMail from "~icons/lucide/mail";
-    import LucideClock from "~icons/lucide/clock";
     import LucidePhoneCall from "~icons/lucide/phone-call";
     import { coursesMap } from "$lib/datas/courses/healpers";
     import LucideYoutube from "~icons/lucide/youtube";
@@ -31,10 +30,8 @@
 >
     <Container padding="top">
         <ContainerContent class="space-y-8">
-            <div
-                class="grid grid-cols-1 gap-x-8 gap-y-6 md:grid-cols-3 lg:grid-cols-2"
-            >
-                <div class="space-y-4">
+            <div class="grid grid-cols-1 gap-x-8 gap-y-6 lg:grid-cols-2">
+                <div class="space-y-8">
                     <div class="h-16">
                         <Picture
                             meta={LogoCotAI}
@@ -45,33 +42,69 @@
                     <p class="text-base-content/80">
                         {m.good_glad_crow_commend()}
                     </p>
-                    <div>
-                        <ExternalAnchor
-                            href="https://www.facebook.com/cotaivn"
-                            aria-label={"Facebook"}
-                            class="btn btn-square"
-                        >
-                            <LucideFacebook />
-                        </ExternalAnchor>
-                        <ExternalAnchor
-                            href="https://www.linkedin.com/company/cotai/mycompany"
-                            aria-label={"Linkedin"}
-                            class="btn btn-square"
-                        >
-                            <LucideLinkedin />
-                        </ExternalAnchor>
-                        <ExternalAnchor
-                            href="https://www.youtube.com/@cotai"
-                            aria-label={"Youtube"}
-                            class="btn btn-square"
-                        >
-                            <LucideYoutube />
-                        </ExternalAnchor>
+
+                    <div class="space-y-8">
+                        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                            <ContactCard title={m.email()}>
+                                <LucideMail
+                                    class="size-5 flex-shrink-0 group-hover:animate-jump"
+                                    slot="icon"
+                                />
+                                <a
+                                    href="mailto:contact@cot.ai"
+                                    class="link-hover link"
+                                    slot="details"
+                                >
+                                    contact@cot.ai
+                                </a>
+                            </ContactCard>
+
+                            <ContactCard title={m.hotline()}>
+                                <LucidePhoneCall
+                                    class="size-5 flex-shrink-0 group-hover:animate-jump"
+                                    slot="icon"
+                                />
+                                <a
+                                    href="tel:+84815503467"
+                                    class="link-hover link"
+                                    slot="details"
+                                >
+                                    {m.phoneNumber()}
+                                </a>
+                            </ContactCard>
+                        </div>
+                        <div class="w-full space-y-4">
+                            <p class="text-sm">
+                                {m.tired_bland_leopard_bubble()}
+                            </p>
+                            <div class="group flex flex-row gap-3">
+                                <LucideMapPin
+                                    class="size-5 flex-shrink-0 group-hover:animate-jump"
+                                />
+                                <ExternalAnchor
+                                    href="https://www.google.com/maps/place/MDN+building/@10.8458901,106.6145654,17z/data=!3m1!4b1!4m5!3m4!1s0x31752b0733df578d:0x176cb06b6ce2d507!8m2!3d10.8459893!4d106.6166801"
+                                    class="link-hover link text-sm"
+                                    slot="details"
+                                >
+                                    {m.contacts_address_1()}
+                                </ExternalAnchor>
+                            </div>
+
+                            <div class="group flex flex-row gap-3">
+                                <LucideMapPin
+                                    class="size-5 flex-shrink-0 group-hover:animate-jump"
+                                />
+                                <ExternalAnchor
+                                    href="https://www.google.com/maps/place//@10.7640773,106.679528,17z/data=!3m1!4b1?entry=ttu"
+                                    class="link-hover link text-sm"
+                                >
+                                    {m.contacts_address_2()}
+                                </ExternalAnchor>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div
-                    class="col-span-1 grid grid-cols-1 gap-x-8 gap-y-6 md:col-span-2 md:grid-cols-2 lg:col-span-1"
-                >
+                <div class="grid grid-cols-1 gap-x-8 gap-y-6 md:grid-cols-2">
                     <div class="space-y-3">
                         <p
                             class="text-sm font-semibold uppercase text-base-content/80"
@@ -156,110 +189,39 @@
                     </div>
                 </div>
             </div>
-            <div class="space-y-4">
-                <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
-                    <ContactCard title={m.email()}>
-                        <LucideMail
-                            class="size-8 flex-shrink-0 group-hover:animate-jump"
-                            slot="icon"
-                        />
-                        <a
-                            href="mailto:contact@cot.ai"
-                            class="link-hover link text-lg font-semibold"
-                            slot="details"
-                        >
-                            contact@cot.ai
-                        </a>
-                    </ContactCard>
-
-                    <ContactCard title={m.hotline()}>
-                        <LucidePhoneCall
-                            class="size-8 flex-shrink-0 group-hover:animate-jump"
-                            slot="icon"
-                        />
-                        <a
-                            href="tel:+84815503467"
-                            class="link-hover link text-lg font-semibold"
-                            slot="details"
-                        >
-                            {m.phoneNumber()}
-                        </a>
-                    </ContactCard>
-
-                    <ContactCard title={m.safe_clear_sparrow_aim()}>
-                        <LucideClock
-                            class="size-8 flex-shrink-0 group-hover:animate-jump"
-                            slot="icon"
-                        />
-                        <p class="text-lg font-semibold" slot="details">
-                            Mon-Fri: 8:00 - 17:30
-                        </p>
-                    </ContactCard>
-                </div>
-                <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
-                    <ContactCard
-                        title={m.tired_bland_leopard_bubble({ idx: 1 })}
-                    >
-                        <LucideMapPin
-                            class="size-8 flex-shrink-0 group-hover:animate-jump"
-                            slot="icon"
-                        />
-                        <ExternalAnchor
-                            href="https://www.google.com/maps/place/MDN+building/@10.8458901,106.6145654,17z/data=!3m1!4b1!4m5!3m4!1s0x31752b0733df578d:0x176cb06b6ce2d507!8m2!3d10.8459893!4d106.6166801"
-                            class="link-hover link text-sm font-semibold"
-                            slot="details"
-                        >
-                            <p>{m.weary_white_mallard_trip()}</p>
-                            <p>
-                                {m.contacts_address_1()}
-                            </p>
-                        </ExternalAnchor>
-                    </ContactCard>
-
-                    <ContactCard
-                        title={m.tired_bland_leopard_bubble({ idx: 2 })}
-                    >
-                        <LucideMapPin
-                            class="size-8 flex-shrink-0 group-hover:animate-jump"
-                            slot="icon"
-                        />
-                        <ExternalAnchor
-                            href="https://www.google.com/maps/place//@10.7640773,106.679528,17z/data=!3m1!4b1?entry=ttu"
-                            class="link-hover link text-sm font-semibold"
-                            slot="details"
-                        >
-                            <p>{m.clean_due_carp_clap()}</p>
-                            <p>
-                                {m.contacts_address_2()}
-                            </p>
-                        </ExternalAnchor>
-                    </ContactCard>
-
-                    <ContactCard
-                        title={m.tired_bland_leopard_bubble({ idx: 3 })}
-                    >
-                        <LucideMapPin
-                            class="size-8 flex-shrink-0 group-hover:animate-jump"
-                            slot="icon"
-                        />
-                        <ExternalAnchor
-                            href="https://www.google.com/maps/place/F%2FC+Coffee/@10.8211744,106.6369177,21z/data=!4m15!1m8!3m7!1s0x3175296504f6eeaf:0x7aee623f96a50e4b!2zOSBOZ3V54buFbiBT4bu5IFPDoWNoLCBQaMaw4budbmcgMTUsIFTDom4gQsOsbmgsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmlldG5hbQ!3b1!8m2!3d10.8230197!4d106.63846!16s%2Fg%2F11f10dh91s!3m5!1s0x3175290acc53959b:0xa1f1474055c95f10!8m2!3d10.821143!4d106.6368483!16s%2Fg%2F11hgdmdyxt?entry=ttu"
-                            class="link-hover link text-sm font-semibold"
-                            slot="details"
-                        >
-                            {m.contacts_address_3()}
-                        </ExternalAnchor>
-                    </ContactCard>
-                </div>
-            </div>
         </ContainerContent>
     </Container>
     <hr class="border-base-content/80" />
     <Container padding="none" class="py-2">
-        <ContainerContent>
+        <ContainerContent
+            class="flex flex-col items-center justify-between gap-2 md:flex-row"
+        >
             <p>
                 © 2019 - {new Date().getFullYear()} CoTAI. All Rights Reserved
             </p>
+            <div>
+                <ExternalAnchor
+                    href="https://www.facebook.com/cotaivn"
+                    aria-label={"Facebook"}
+                    class="btn btn-square"
+                >
+                    <LucideFacebook />
+                </ExternalAnchor>
+                <ExternalAnchor
+                    href="https://www.linkedin.com/company/cotai/mycompany"
+                    aria-label={"Linkedin"}
+                    class="btn btn-square"
+                >
+                    <LucideLinkedin />
+                </ExternalAnchor>
+                <ExternalAnchor
+                    href="https://www.youtube.com/@cotai"
+                    aria-label={"Youtube"}
+                    class="btn btn-square"
+                >
+                    <LucideYoutube />
+                </ExternalAnchor>
+            </div>
         </ContainerContent>
     </Container>
 </footer>
