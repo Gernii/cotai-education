5177a54f40b9a736c58bf4f7d45ec878b06f7edb
Le Ngo Duc Anh <anhducngole2001@gmail.com>
2024-07-18 15:09:00 +0700

    fix(web): hero roadmap show wrong star numbers (#99)

--- apps/web/src/lib/features/hero-roadmap/re-organize-courses.ts
+++ apps/web/src/lib/features/hero-roadmap/re-organize-courses.ts
@@ -2,12 +2,19 @@ import type { HeroRoadmapCourse } from "./types";
 
 export const reorganizeCourses = (courses: HeroRoadmapCourse[]) => {
     const reorganizedCourses = courses.reduce((prev, course) => {
+        const treeShakedCourse: HeroRoadmapCourse = {
+            id: course.id,
+            title: course.title,
+            shortTitle: course.shortTitle,
+            totalStars: course.totalStars,
+        };
         const currList = prev.pop() ?? [];
+
         if (currList.length < 2) {
-            currList.push(course);
+            currList.push(treeShakedCourse);
             prev.push(currList);
         } else {
-            prev.push(currList, [course]);
+            prev.push(currList, [treeShakedCourse]);
         }
         return prev;
     }, [] as HeroRoadmapCourse[][]);
