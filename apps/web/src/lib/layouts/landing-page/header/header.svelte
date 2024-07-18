69cadad3c033efa0d636806ce821f39300b3d827
Le Ngo Duc Anh <anhducngole2001@gmail.com>
2024-07-18 11:13:58 +0700

    refactor(web): refactor homepage layout head (#89)

--- apps/web/src/lib/layouts/landing-page/header/header.svelte
+++ apps/web/src/lib/layouts/landing-page/header/header.svelte
@@ -4,6 +4,7 @@
     import LogoCotAI from "$lib/assets/logo/CoTAI-Ver0-640.png?imagetools";
 
     import { Picture } from "$lib/components/ui/picture";
+    import { Container, ContainerContent } from "$lib/components/ui/container";
 
     import * as m from "$i18n/messages";
 
@@ -44,26 +45,30 @@
 <svelte:window on:scroll={onScrollHandler} />
 
 <header
-    class="fixed inset-x-1 top-1 z-40 h-header-sm rounded-box border border-base-content/5 bg-base-200 px-1 py-1 shadow-xl lg:h-header lg:px-2"
+    class="fixed inset-x-0 top-0 z-40 bg-base-200 px-3 py-2 shadow-xl lg:px-6 lg:py-4"
 >
-    <div class="flex h-full items-center gap-x-2 px-2 lg:gap-x-8">
-        <div class="block lg:hidden">
-            <SidenavTrigger />
-        </div>
-        <a
-            href={"/"}
-            class="absolute left-1/2 w-24 -translate-x-1/2 lg:static lg:translate-x-0"
-        >
-            <Picture meta={LogoCotAI} alt={m.mainLogo()} />
-        </a>
-        <div class="hidden lg:block">
-            <Navigator />
-        </div>
-        <div class="flex flex-grow-0 lg:flex-grow" />
-        <div class="hidden lg:flex">
-            <!-- TODO: re-open after update ENG language   -->
-            <!-- <LangSelector /> -->
-            <!-- <ThemeSelector /> -->
-        </div>
-    </div>
+    <Container padding="none">
+        <ContainerContent>
+            <div class="flex h-full items-center gap-x-2 px-2 lg:gap-x-8">
+                <div class="block lg:hidden">
+                    <SidenavTrigger />
+                </div>
+                <a
+                    href={"/"}
+                    class="absolute left-1/2 w-24 -translate-x-1/2 lg:static lg:translate-x-0"
+                >
+                    <Picture meta={LogoCotAI} alt={m.mainLogo()} />
+                </a>
+                <div class="flex flex-grow-0 lg:flex-grow" />
+                <div class="hidden lg:block">
+                    <Navigator />
+                </div>
+                <!-- <div class="hidden lg:flex"> -->
+                <!-- TODO: re-open after update ENG language   -->
+                <!-- <LangSelector /> -->
+                <!-- <ThemeSelector /> -->
+                <!-- </div> -->
+            </div>
+        </ContainerContent>
+    </Container>
 </header>
